29436c9ad54f190a6e2ea7b6509367e6eefe5c7c
kagashino <ghosthcp516@hotmail.com>
2020-05-29 14:33:44 +0800

    fix: use hardcode manifests, extract vendors

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,18 @@
 module.exports = (defaultConfig) => {
   const config = { ...defaultConfig };
+
+
+  config.optimization.splitChunks = {
+    ...config.optimization.splitChunks,
+    cacheGroups: {
+      commons: {
+        test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+      }
+    }
+  };
+
   config.resolve.alias = {
     api: 'assets/api',
     assets: 'assets',
--- src/container/Sidebar.tsx
+++ src/container/Sidebar.tsx
@@ -1,47 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import SubApps from '../assets/routes/subApps';
 import './styles/Sidebar.scss';
-import Loading from '../components/Loading';
-import { AppContext } from '../assets/store/context';
+
 
 interface UseAppReturnValue {
-  loading: boolean,
   subApps: Partial<SubApp>[]
 }
 
 const useSubApps = (): UseAppReturnValue => {
-  const { Api } = useContext(AppContext);
-  const [loading, setLoading] = useState(true);
-  const [subApps, setSubApps] = useState<Partial<SubApp>[]>([]);
-
-  useEffect(() => {
-    if (!subApps.length) {
-      Api.Manifest.list().then((res: any[]) => {
-        setSubApps(res);
-        setLoading(false);
-      });
-    }
-  }, [Api, loading, subApps, subApps.length]);
+  const [subApps] = useState<Partial<SubApp>[]>(SubApps);
 
   return {
-    loading,
     subApps,
   } as UseAppReturnValue;
 };
 
 export default function Sidebar() {
-  const { loading, subApps } = useSubApps();
+  const { subApps } = useSubApps();
 
   return (
     <aside className="sidebar">
       <h3>功能</h3>
       {
-        loading
-          ? <Loading />
-          : subApps.map(({ name, title }) => (
-            <NavLink key={name} to={`/app/${name}`}>{ title }</NavLink>
-          ))
+        subApps.map(({ name, title }) => (
+          <NavLink key={name} to={`/app/${name}`}>{ title }</NavLink>
+        ))
       }
     </aside>
   );
